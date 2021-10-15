1d68bbe3f0974c100b158b737ccced98a81e4c8c
Samirazzaq <samirazzaq8@gmail.com>
2021-10-15 06:55:35 -0700

    calculator python

---
+++ calculator python.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+#Calculator
+val1=int(input("Enter value no1"));
+val2=int(input("Enter value no2"));
+operator=input("Enter operator");
+
+if operator == ("+"):
+    val=val1+val2
+    print(val,"Answer")
+elif operator == ("-"):
+    val=val1-val2
+    print(val,"Answer")
+elif operator == ("*"):
+    val=val1*val2
+    print(val,"Answer")
+elif operator == ("/"):
+    val=val1/val2
+    print(val,"Answer")
+else:
+    print("Incorrect Operator")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
